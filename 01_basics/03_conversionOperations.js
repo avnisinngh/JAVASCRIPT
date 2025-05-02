97f9ffba3dbea20c2b4d9dfea7e556798fd87a85
Avni Singh <150375257+avnisinngh@users.noreply.github.com>
2025-05-02 04:36:22 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+// let score = true
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33 
+// "33abs" => NaN
+// null => 0
+// false => 0
+// true => 1
+// undefined => NaN
+
+// let isLoggedIn = "asa"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true, 0=> false
+// "" => false
+// "sad" => true
+
+
