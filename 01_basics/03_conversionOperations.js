7171fb8e35ec9243d0c9f91d5654d1f4e02118c4
Avni Singh <150375257+avnisinngh@users.noreply.github.com>
2025-05-03 04:16:37 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,3 +24,31 @@
 // "sad" => true
 
 
+// ************************ OPERATIONS ****************************
+
+let value = 3
+// let negValue = -value
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// console.log("1" + 2 );
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+
+// console.log(+" ");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1);
+
