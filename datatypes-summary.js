1fdffeed7a731fbf93d068c99cd3d69119c79954
Avni Singh <150375257+avnisinngh@users.noreply.github.com>
2025-05-03 05:40:07 +0000

    day 2

---
+++ datatypes-summary.js
@@ -0,0 +1,11 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+// Refrence (Non- Primitive)
+
+//Array, objects, Functions
