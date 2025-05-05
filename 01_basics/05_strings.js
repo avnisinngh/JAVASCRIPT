c27d0e77e1b2270cc306d3e67af9e776c4a1e548
Avni Singh <150375257+avnisinngh@users.noreply.github.com>
2025-05-05 01:00:48 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "avni"
+const repoCount = 50
+
+// console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('avni-ac')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+const newString = gameName.substring(-8,4)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8,2);
+console.log(anotherString);
+
+const newString1 = "  avni  "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://avni.com/avni%20avnii"
+
+
+
+
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -5,7 +5,34 @@
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 // Refrence (Non- Primitive)
 
 //Array, objects, Functions
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(primitive), Heap(Non=Primitive)
+
+let myYoutubename = "avnidotcom"
+let anothername = myYoutubename
+anothername = "codecasdemia"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email : "user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
